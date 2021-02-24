edcda1ae28c499938335afe793fec8b5706ad158
GabrielSilva2y3d <74156876+GabrielSilva2y3d@users.noreply.github.com>
2021-02-24 18:11:44 -0300

    Conhecendo variaveis, constantes e condicionais

--- Hello_World-PHP/index.php
+++ Hello_World-PHP/index.php
@@ -37,7 +37,7 @@
 			?>
 
 		<h1> Constantes </h1>
-		<h2>Criando uma constante</h2>
+		<h2>Criando constantes</h2>
 			<?php
 				/* Declarar uma constante case sensitive */
 				define("constanteCaseSensitive","CASE_SENSITIVE");
@@ -45,6 +45,122 @@
 				echo "<br/>";
 				define("constanteNaoCaseSensitive","NAO_CASE_SENSITIVE", True);
 				echo constanteNaoCaseSensitive;
+				echo"<br/>";
+
+				/* Outra forma de Declarar uma constante */
+				const  CONSTANT = ' Constante com "const"';
+				echo CONSTANT;
+				echo "<br/>";
+
+				/* Constante com Array */
+				const Pokemon = array('Pikachu','Lizardon','Gekkouga','Eve');
+				for ($cont = 0; $cont <= 4;$cont++){
+					echo Pokemon[$cont].", ";
+				} 
+				
 			?>
+
+			<br/>
+
+			<h1>Condicionais IF, ELSE, SWITCH, WHILE E FOR</h3>
+			<h2>If e Else</h2>
+			<?PHP
+				/*De acordo com o valor da variavel $saldoBancario
+				o resultado do programa pode variar */
+				echo "<p>SsaldoBancario = 42389.50;<br/>
+				if(SsaldoBancario > 42390){<br/>
+					echo 'Vou comprar um carro 0KM :D';<br/>
+				}else{<br/>
+					echo 'Vou comprar um carro usado :/';<br/>
+				}</p>";
+
+				$saldoBancario = 42389.50;
+				if($saldoBancario > 42390){
+					echo "Vou comprar um carro 0KM";
+				}else{
+					echo "Vou comprar um carro usado";
+				}
+			?>
+		
+				
+				<h2>Switch</h2>
+				<?PHP
+					/*Mensagem de erro/sucesso para a validação 
+					de formulários. 
+					O algoritimo monta a mensagem de erro de
+					acordo com a quantidade de erros encontrada*/
+
+					echo "<p>SquantidadeErros = 3;<br/>
+					switch(SquantidadeErros){<br/>
+						case 0:<br/>
+							SmensagemDeErro = 'Nenhum';<br/>
+							break;<br/>
+							<br/>
+						case 1:<br/>
+							SmensagemDeErro = 'Um';<br/>
+							break;<br/>
+							<br/>
+						case 2:<br/>
+							SmensagemDeErro = 'Dois';<br/>
+							break;<br/>
+							<br/>
+						default:<br/>
+							SmensagemDeErro = 'Mais de dois';<br/>	
+							<br/>
+					}<br/>
+					SmensagemDeErro .= 'erro(s) encontrado(s)';<br/>
+					echo SmensagemDeErro;</p>";
+
+					$quantidadeErros = 3; //tem que ser maior ou igual a 0
+					switch($quantidadeErros){
+						case 0:
+							$mensagemDeErro = "Nenhum";
+							break;
+						
+						case 1:
+							$mensagemDeErro = "Um";
+							break;
+						
+						case 2:
+							$mensagemDeErro = "Dois";
+							break;
+
+						default:
+							$mensagemDeErro = "Mais de dois";	
+
+					}
+					$mensagemDeErro .= " erro(s) encontrado(s)";
+					echo $mensagemDeErro;
+				?>
+				<br/>
+				<h2>While</h2>
+				<?PHP
+				/*O while recebe como parâmetro um valor booleano
+				e permanece em looping até quando a condição for
+				verdadeira*/
+				echo "<p>Scont = 0;<br/>
+				while(Scont++ < 10) {<br/>
+					echo Scont;<br/>
+				}</p>";
+				$cont = 0;
+				while($cont++ < 10) {
+					echo $cont."<br/>";
+				}
+				?>
+				<br/>
+				<h2>For</h2>
+				<?PHP
+				/*O for recebe 3 parâmetros separados por ponto e virgula
+				o primeiro é a inicialização do contador
+				o segundo é um booleano que define o final do contador
+				o terceiro é o incremento para o contador */
+				echo "<p>for(Scont = 1; Scont <= 10; Scont++){<br>
+					echo Scont<br>
+				}</p>";
+				for($cont = 1; $cont <= 10; $cont++){
+					echo $cont."<br/>";
+				}
+				
+				?>
 	</body>
 </html>
